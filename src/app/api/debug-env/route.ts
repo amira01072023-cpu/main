ca56eb4a6c284ed94c2311b01c1bb4e78f205149
Amira <amira.01072023@gmail.com>
2026-03-11 01:37:07 +0400

    Fix debug-env API route path

---
+++ src/app/api/debug-env/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const service = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+return NextResponse.json({
+hasUrl: !!url,
+hasAnon: !!anon,
+hasService: !!service,
+urlHost: url ? new URL(url).host : null,
+});
+}
